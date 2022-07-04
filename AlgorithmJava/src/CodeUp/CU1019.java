297f59ca4b184b50c818d32e904551733776d5c5
lkc263 <lkc263@naver.com>
2022-07-04 18:07:21 +0900

    ✏ CU 1019

--- AlgorithmJava/src/CodeUp/CU1019.java
+++ AlgorithmJava/src/CodeUp/CU1019.java
@@ -1,18 +1,16 @@
 package CodeUp;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class CU1019 {
     public static void main(String[] args) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String s = br.readLine();
-            String[] rs = s.split(".");
-            for (String r : rs) {
-                System.out.println("r = " + r);
-            }
-//            System.out.println("list_s = " + rs[0]);
-//            System.out.format("%d.%02d.%02d",list_s[0],list_s[1],list_s[2]);
+            String[] rs = s.split("\\.");
+            int[] arr = Arrays.stream(rs).mapToInt(Integer::parseInt).toArray();
+            System.out.format("%04d.%02d.%02d",arr[0],arr[1],arr[2]);
 
             br.close();
         } catch (Exception e) {
