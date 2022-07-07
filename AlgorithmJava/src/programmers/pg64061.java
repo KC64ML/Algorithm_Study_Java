cb9cd158d0b37dcc1e1cc7572d1854a05880fabd
lkc263 <lkc263@naver.com>
2022-07-07 18:07:42 +0900

    ✏ programmers 64061

---
+++ AlgorithmJava/src/programmers/pg64061.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.io.*;
+
+public class pg64061 {
+    public static void main(String[] args) throws IOException{
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] inputs = br.readLine().split("");
+
+
+
+
+        br.close();
+        bw.close();
+    }
+}
