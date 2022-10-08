b141808ea8b2e77dd7be80873d9362a6aa49215f
lkc263 <lkc263@naver.com>
2022-10-08 18:11:52 +0900

    no message

--- AlgorithmJava/src/SWExpertAcademy/sw1979.java
+++ AlgorithmJava/src/SWExpertAcademy/sw1979.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class sw1979 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println(s.length());
+        int num = sc.nextInt();
+        for(int i=0;i<num;i++){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[][] arr = new int[n+2][n+2];
+
+            for(int j=0;j<n;j++){
+                for(int q=0; q <n;q++){
+                    arr[j][q] = sc.nextInt();
+                }
+            }
+
+
+        }
+
     }
 }
