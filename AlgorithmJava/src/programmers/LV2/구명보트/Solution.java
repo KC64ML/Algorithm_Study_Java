9669de382f5c1da28efd636b06c4eb5cdf6cf3fe
이경창 <lkc263@naver.com>
2022-12-15 13:35:44 +0900

    ✏️ 2022.12.15 N개의 최소공배수

--- AlgorithmJava/src/programmers/LV2/구명보트/Solution.java
+++ AlgorithmJava/src/programmers/LV2/구명보트/Solution.java
@@ -4,53 +4,53 @@ import java.util.*;
 
 
 // 효율성이 좋지 못한 코드 75점
-class Solution {
-
-    int[] people;
-    int limit;
-    int answer;
-
-    int swap(int a, int b){
-        return a;
-    }
-
-
-    void boat(int startIdx, int lastIdx, int cnt){
-        // System.out.println("start : " + startIdx + " lastIdx : " + lastIdx + "cnt : " + cnt);
-        int sum = people[startIdx];
-        int i = lastIdx;
-        for(; i > startIdx; i--){
-            // System.out.println("i : " + i + " people" + people[i]);
-            if(sum + people[i] <= limit) sum += people[i];
-            else break;
-        }
-
-        if(i == startIdx){
-            answer = cnt;
-            return;
-        }else{
-            boat(startIdx+1, i, cnt + 1);
-        }
-    }
-
-    public int solution(int[] _people, int _limit) {
-
-        people = _people;
-        limit = _limit;
-        // 역정렬
-        for(int i =0; i< people.length; i++){
-            for(int j =i+1; j < people.length; j++){
-                if(people[i] < people[j]) people[j] = swap(people[i], people[i]=people[j]);
-            }
-        }
-
-        if(people.length > 1 && (people[people.length-1] + people[people.length-2] > _limit)) return people.length;
-
-        boat(0, people.length - 1, 1);
-
-        return answer;
-    }
-}
+//class Solution {
+//
+//    int[] people;
+//    int limit;
+//    int answer;
+//
+//    int swap(int a, int b){
+//        return a;
+//    }
+//
+//
+//    void boat(int startIdx, int lastIdx, int cnt){
+//        // System.out.println("start : " + startIdx + " lastIdx : " + lastIdx + "cnt : " + cnt);
+//        int sum = people[startIdx];
+//        int i = lastIdx;
+//        for(; i > startIdx; i--){
+//            // System.out.println("i : " + i + " people" + people[i]);
+//            if(sum + people[i] <= limit) sum += people[i];
+//            else break;
+//        }
+//
+//        if(i == startIdx){
+//            answer = cnt;
+//            return;
+//        }else{
+//            boat(startIdx+1, i, cnt + 1);
+//        }
+//    }
+//
+//    public int solution(int[] _people, int _limit) {
+//
+//        people = _people;
+//        limit = _limit;
+//        // 역정렬
+//        for(int i =0; i< people.length; i++){
+//            for(int j =i+1; j < people.length; j++){
+//                if(people[i] < people[j]) people[j] = swap(people[i], people[i]=people[j]);
+//            }
+//        }
+//
+//        if(people.length > 1 && (people[people.length-1] + people[people.length-2] > _limit)) return people.length;
+//
+//        boat(0, people.length - 1, 1);
+//
+//        return answer;
+//    }
+//}
 
 
 // 답안 코드
