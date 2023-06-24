31749cf99af369a5be9ede39f7c9aca1af96b5fb
이경창 <lkc263@naver.com>
2023-06-24 22:41:20 +0900

    모음 사전 최적화 (Lv2)

---
+++ AlgorithmJava/src/programmers/LV2/모음_사전_최적화/Solution.java
@@ -0,0 +1,23 @@
+package programmers.LV2.모음_사전_최적화;
+
+import java.util.*;
+
+public class Solution {
+
+    private static final char[] CHARS = "AEIOU".toCharArray();
+
+    private void generate(String word, List<String> words){
+        words.add(word);
+
+        if (word.length() == 5) return;
+        for (char c : CHARS) {
+            generate(word + c, words);
+        }
+    }
+
+    public int solution(String word) {
+        List<String> words = new ArrayList<>();
+        generate("", words);
+        return words.indexOf(word);
+    }
+}
