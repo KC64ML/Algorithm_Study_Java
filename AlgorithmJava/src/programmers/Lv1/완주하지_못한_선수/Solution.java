8d3ed54c76a981efea584fad28757d8b69bd0ee1
이경창 <lkc263@naver.com>
2023-07-01 17:30:40 +0900

    완주하지 못한 선수

---
+++ AlgorithmJava/src/programmers/Lv1/완주하지_못한_선수/Solution.java
@@ -0,0 +1,23 @@
+package programmers.Lv1.완주하지_못한_선수;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> count = new HashMap<>();
+
+        for(String name : participant){
+            count.putIfAbsent(name, 0);
+            count.put(name, count.get(name) + 1);
+        }
+
+        for(String name : completion) {
+            int v = count.get(name) - 1;
+            count.put(name, v);
+            if(v == 0) count.remove(name);
+        }
+
+        return count.keySet().iterator().next();
+    }
+}
