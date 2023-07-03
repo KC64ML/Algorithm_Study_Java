704785b79b290b020fd76bb6110061a7d96982cf
이경창 <lkc263@naver.com>
2023-07-03 15:48:39 +0900

    등굣길

---
+++ AlgorithmJava/src/programmers/LV3/등굣길/Solution.java
@@ -0,0 +1,33 @@
+package programmers.LV3.등굣길;
+
+import java.util.*;
+
+public class Solution {
+
+    private int[][] mem = new int[101][101];
+
+    private int count(int x, int y, int w, int h, boolean[][] isPuddle){
+        if (x > w || y > h) return 0;
+        if (isPuddle[y][x]) return 0;
+
+        if(mem[x][y] != -1) return mem[x][y];
+        if (x == w && y == h) return 1;
+
+        int total = count(x + 1, y, w, h, isPuddle) + count(x, y + 1, w, h, isPuddle);
+        return mem[x][y] = total % 1000000007;
+    }
+
+    public int solution(int m, int n, int[][] puddles) {
+        boolean[][] isPuddle = new boolean[n + 1][m + 1];
+
+        for(int[] row : mem){
+            Arrays.fill(row, -1);
+        }
+
+        for(int[] p : puddles){
+            isPuddle[p[1]][p[0]] = true;
+        }
+
+        return count(1, 1, m, n, isPuddle);
+    }
+}
