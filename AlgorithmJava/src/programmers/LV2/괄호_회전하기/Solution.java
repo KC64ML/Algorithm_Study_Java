c3748332acfe58bc42a92b145c26b61d35caf6aa
이경창 <lkc263@naver.com>
2023-07-03 16:43:16 +0900

    괄호 회전하기

---
+++ AlgorithmJava/src/programmers/LV2/괄호_회전하기/Solution.java
@@ -0,0 +1,42 @@
+package programmers.LV2.괄호_회전하기;
+
+import java.util.Stack;
+
+public class Solution {
+
+    private boolean isCorrect(char[] str, int offset){
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < str.length; i++){
+            char c = str[(offset + i) % str.length];
+
+            switch(c) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                case ')', '}', ']':
+                    if(stack.isEmpty()) return false;
+                    if(stack.pop() != c) return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public int solution(String s) {
+        char[] str = s.toCharArray();
+        int count = 0;
+        for(int offset = 0; offset < str.length; offset++){
+            if(isCorrect(str, offset)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
