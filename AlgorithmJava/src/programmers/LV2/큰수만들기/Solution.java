a6937e6a9f0ff3774b5cc2281e18f0878859e1c3
이경창 <lkc263@naver.com>
2023-07-06 11:27:30 +0900

    큰수만들기

---
+++ AlgorithmJava/src/programmers/LV2/큰수만들기/Solution.java
@@ -0,0 +1,25 @@
+package programmers.LV2.큰수만들기;
+
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public String solution(String number, int k) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : number.toCharArray()){
+            while (k > 0 && !stack.isEmpty() && c > stack.peek()){
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
+        }
+
+        while (k-- > 0){
+            stack.pop();
+        }
+
+        return stack.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+}
