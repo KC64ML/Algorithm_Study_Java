4e4b868685b4ee782af8a565ff72a04939c260c1
이경창 <lkc263@naver.com>
2023-07-21 12:15:42 +0900

    수식 최대화

--- AlgorithmJava/src/programmers/LV2/수식_최대화/Solution3.java
+++ AlgorithmJava/src/programmers/LV2/수식_최대화/Solution3.java
@@ -1,20 +1,66 @@
 package programmers.LV2.수식_최대화;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution3 {
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
 
-        list.remove(1);
-        list.add(1);
+    private String[][] CODE = {
+            "*+-".split(""),
+            "*-+".split(""),
+            "+*-".split(""),
+            "+-*".split(""),
+            "-+*".split(""),
+            "-*+".split("")
+    };
 
-        System.out.println(Arrays.toString(list.toArray()));
+    private long calculate(long number1, long number2, String inCode){
+        switch(inCode){
+            case "*":
+                return number1 * number2;
+            case "+":
+                return number1 + number2;
+            case "-":
+                return number1 - number2;
+            default:
+                return 0;
+        }
+    }
+
+    private long calculate(String[] inArrCode, List<String> list){
+        for(String inCode : inArrCode){
+            for(int i = 0; i < list.size(); i++){
+                if(list.get(i).equals(inCode)){
+                    long number1 = Long.parseLong(list.get(i - 1));
+                    long number2 = Long.parseLong(list.get(i + 1));
+                    long nextData = calculate(number1, number2, list.get(i));
+                    list.remove(i - 1);
+                    list.remove(i - 1);
+                    list.remove(i - 1);
+                    // System.out.println("left : " + number1 + " , right : " + number2  + " 결과 : " + nextData);
+                    list.add(i - 1, Long.toString(nextData));
+                    i -= 2;
+                }
+            }
+        }
+
+
+        return Math.abs(Long.parseLong(list.get(0)));
+    }
+
+    public long solution(String expression) {
+        StringTokenizer tokenizer = new StringTokenizer(expression, "*+-", true);
+
+        List<String> list = new ArrayList<>();
+        long answer = 0;
+
+        while(tokenizer.hasMoreTokens()){
+            list.add(tokenizer.nextToken());
+        }
+
+        for(String[] inArrCode : CODE){
+            answer = Math.max(answer, calculate(inArrCode, new ArrayList<>(list)));
+        }
+
+        return answer;
     }
 }
