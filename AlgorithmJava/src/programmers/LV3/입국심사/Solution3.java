246fc2f60ef2b71c4b6df718ed4a06026889884c
이경창 <lkc263@naver.com>
2023-07-22 13:32:08 +0900

    입국 심사

---
+++ AlgorithmJava/src/programmers/LV3/입국심사/Solution3.java
@@ -0,0 +1,23 @@
+package programmers.LV3.입국심사;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 5, 7, 9, 11};
+        int target = 5;
+
+        int start = 0;
+        int end = arr.length;
+        int answer = 0;
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(arr[mid] == 5){
+                answer = mid;
+                break;
+            }
+            else if(arr[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        System.out.println(answer);
+    }
+}
