1e129ce1d8a1d3d2cd40a1f2f595189c24b6e315
이경창 <lkc263@naver.com>
2023-07-25 16:21:33 +0900

    없는 숫자 더하기

--- AlgorithmJava/src/programmers/Lv1/없는숫자_더하기/Solution2.java
+++ AlgorithmJava/src/programmers/Lv1/없는숫자_더하기/Solution2.java
@@ -2,9 +2,17 @@ package programmers.Lv1.없는숫자_더하기;
 
 import java.util.Arrays;
 
+
+
 public class Solution2 {
-    public static void main(String[] args) {
-        int[] number = {1,2,3,4,5,6,7,8};
-        Arrays.stream(number);
+    public int solution(int[] numbers) {
+        
+        // 1 ~ 10 : 55
+        int sum = 45;
+
+        for(int num : numbers) sum -= num;
+
+
+        return sum;
     }
 }
