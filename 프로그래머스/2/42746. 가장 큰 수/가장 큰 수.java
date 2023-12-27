0d73ed708d61d769a8a27c599deb16398e74fb69
KyoungChang Lee <72541544+KC64ML@users.noreply.github.com>
2023-12-27 13:02:06 +0900

    [level 2] Title: 가장 큰 수, Time: 5.52 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,19 +1,42 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
+    // Arrays.strea().mapToInt().
     public String solution(int[] numbers) {
-        String s = Arrays.stream(numbers).mapToObj(String::valueOf)
-            .sorted((num1, num2) -> {
-                int original = Integer.parseInt(num1 + num2);
-                int second = Integer.parseInt(num2 + num1);
+        numbers = Arrays.stream(numbers).boxed().sorted((num1, num2) -> {
+            char[] c1 = Integer.toString(num1).toCharArray();
+            char[] c2 = Integer.toString(num2).toCharArray();
+            
+            // 두 수사이에 작은 것이 있다면
+            for(int i = 0; i < Math.min(c1.length, c2.length); i++){
+                if(c1[i] != c2[i]) return c2[i] - c1[i];
+            }
+            
+            if(c1.length == c2.length) return 0;
+            
+            // 길이가 한쪽이 길고, 같은 값으로만 이루어져 있다면
+            for(int i = Math.min(c1.length, c2.length); i < Math.max(c1.length, c2.length); i++){
+                int minIdx = i - Math.min(c1.length, c2.length);
                 
-                return second - original;
-            }).collect(Collectors.joining(""));
-        // replace()
+                // if(minIdx >= Math.min(c1.length, c2.length)) break;
+                // c2를 검토
+                if(c1.length < c2.length){
+                    if(c1[minIdx] != c2[i]) return c2[i] - c1[minIdx];
+                }else if(c1.length > c2.length){
+                    if(c1[i] != c2[minIdx]) return c2[minIdx] - c1[i];
+                }
+            }
+            
+            return c2.length - c1.length;
+
+        }).mapToInt(Integer::intValue).toArray();
         
-        if(s.charAt(0) == '0') return "0";
-        // System.out.println(s);
-        return s;
+        
+        StringBuilder builder = new StringBuilder();
+        for(int num : numbers){
+            builder.append(num);
+        }
+        
+        return builder.toString();
     }
 }
