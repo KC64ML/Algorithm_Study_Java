8ba23c55104179b45add0718c9749700a24373c5
KyoungChang Lee <72541544+KC64ML@users.noreply.github.com>
2024-02-13 15:39:37 +0900

    [Silver IV] Title: 토너먼트, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int N, start, end;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        N = Integer.parseInt(tokenizer.nextToken());
+        start = Integer.parseInt(tokenizer.nextToken());
+        end = Integer.parseInt(tokenizer.nextToken());
+
+        int count = 0;
+
+        while(start != end){
+            start = start / 2 + start % 2;
+            end = end / 2 + end % 2;
+            count++;
+        }
+
+        System.out.println(count);
+
+        reader.close();
+    }
+}
