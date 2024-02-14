25af49f335261f748e9f16a9f048335fc0789450
KyoungChang Lee <72541544+KC64ML@users.noreply.github.com>
2024-02-14 16:20:43 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 104 ms, Memory: 16212 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+
+    private static String s;
+    private static int N;
+    private static int number;
+    private static int index;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        s = reader.readLine();
+
+        while(true){
+            if(index == s.length()) break;
+            else{
+                number++;
+                char[] numberChar = String.valueOf(number).toCharArray();
+
+                // cArr 잘라야 함
+
+                char[] cArr;
+                if(index + numberChar.length > s.length()){
+                    cArr = s.substring(index, s.length()).toCharArray();
+                }else{
+                    cArr = s.substring(index, index + numberChar.length).toCharArray();
+                }
+
+                int i = 0;
+                int i2 = 0;
+                for(; i < numberChar.length && i2 < cArr.length; i++){
+                    if(cArr[i2] == numberChar[i]){
+                        i2++;
+                    }
+                }
+
+                index += i2;
+            }
+
+        }
+
+        System.out.println(number);
+
+        reader.close();
+    }
+}
